92b181e779497bd32e306d7a2a3d13fbb3b843f6
Daniel Zheng <daniel.zheng@barnebys.com>
2021-04-07 01:06:38 +0200

    added titan and eren

---
+++ GrappleOverTitan/Assets/Scripts/Titan.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Titan : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

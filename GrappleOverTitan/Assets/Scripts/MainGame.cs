38185b634212b95f0ed6a537d260717c744eb2b6
Daniel Zheng <daniel.zheng@barnebys.com>
2021-04-06 02:57:09 +0200

    add map generation

--- GrappleOverTitan/Assets/Scripts/MainGame.cs
+++ GrappleOverTitan/Assets/Scripts/MainGame.cs
@@ -10,6 +10,8 @@ public class MainGame : MonoBehaviour
     public GameObject treeBranchPrefab;
 
     private GameObject grapplingHookObject;
+    private float titanSpawnX = 15;
+    private float treeSpawnX = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +21,14 @@ public class MainGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var playerObject = GameObject.FindWithTag("Player");
+
         if (Input.GetMouseButtonDown(0))
         {
             var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
             if(hit.collider != null)
             {
-                Debug.Log("hit");
-                var playerObject = GameObject.FindWithTag("Player");
-                
                 if(playerObject != null)
                 {
                     Debug.DrawLine(playerObject.transform.position, hit.point, Color.white, 2.5f);
@@ -45,6 +46,24 @@ public class MainGame : MonoBehaviour
             {
                 Destroy(grapplingHookObject);
             }
+        }
+
+        if(playerObject != null && playerObject.transform.position.x + 20 >= titanSpawnX)
+        {
+            var titanObject = Instantiate(titanPrefab,
+                new Vector3(titanSpawnX, Random.value * 4 - 7, 0),
+                Quaternion.identity);
+
+            titanSpawnX += 5 + Random.value * 5;
+        }
+
+        if(playerObject != null && playerObject.transform.position.x + 20 >= treeSpawnX)
+        {
+            var treeBranchObject = Instantiate(treeBranchPrefab,
+                new Vector3(treeSpawnX, 5 - Random.value * 4, 0),
+                Quaternion.identity);
+
+            treeSpawnX += 5 + Random.value * 5;
         }
     }
 }

e8e6b46792519ad0a74f3d3f5226e66d3a13e201
Daniel Zheng <daniel.zheng@barnebys.com>
2021-04-08 02:56:39 +0200

    fix ground titan bug

--- GrappleOverTitan/Assets/Ground.cs
+++ GrappleOverTitan/Assets/Ground.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player")
         {

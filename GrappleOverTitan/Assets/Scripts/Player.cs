a8cb325199d3d5f83fb849a430c17fbfb2103c70
Daniel Zheng <daniel.zheng@barnebys.com>
2021-04-12 04:54:21 +0200

    added body parts

--- GrappleOverTitan/Assets/Scripts/Player.cs
+++ GrappleOverTitan/Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject bloodPrefab;
+    public GameObject headPrefab;
+    public GameObject footPrefab;
+    public GameObject bodyPrefab;
+    public GameObject swordPrefab;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,8 +17,44 @@ public class Player : MonoBehaviour
             Destroy(this.gameObject);
             var bloodObject = Instantiate(bloodPrefab,
                 this.transform.position,
+                Random.rotation
+            );
+            
+            var head = Instantiate(headPrefab,
+                this.transform.position,
+                Random.rotation
+            );
+            head.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * 100);
+            
+            var foot1 = Instantiate(footPrefab,
+                this.transform.position,
+                Random.rotation
+            );
+            foot1.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * 100);
+
+            var foot2 = Instantiate(footPrefab,
+                this.transform.position,
+                Random.rotation
+            );
+            foot2.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * 100);
+            
+            var body = Instantiate(bodyPrefab,
+                this.transform.position,
                 this.transform.rotation
             );
+            body.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * 100);
+            
+            var sword1 = Instantiate(swordPrefab,
+                this.transform.position,
+                this.transform.rotation
+            );
+            sword1.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * 100);
+
+            var sword2 = Instantiate(swordPrefab,
+                this.transform.position,
+                this.transform.rotation
+            );
+            sword2.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * 100);
         }
     }
 }
--- GrappleOverTitan/Assets/Scripts/TimedDestroy.cs
+++ GrappleOverTitan/Assets/Scripts/TimedDestroy.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blood : MonoBehaviour
+public class TimedDestroy : MonoBehaviour
 {
+    public float seconds;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,6 @@ public class Blood : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Destroy(this.gameObject, 60);
+        Destroy(this.gameObject, seconds);
     }
 }
